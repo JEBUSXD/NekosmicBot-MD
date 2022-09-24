33f34d9653ef1be00e7c6e53d763dbb4ed7270ad
Yesenia57 <113145347+Yesenia57@users.noreply.github.com>
2022-09-23 19:48:26 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -37,7 +37,7 @@ let handler = async (m, { conn, usedPrefix: _p, __dirname, command }) => {
     var pweb = await conn.profilePictureUrl(conn.user.jid).catch(_ => './multimedia/imagenes/avatar_contact.png')
     var textweb = ''
     }
-    const message = m.reply(` ```CARGANDO MENU DEL BOT``` ${textweb}`)
+    const message = m.reply(` ```CARGANDO MENU DEL BOT🔄``` ${textweb}`)
     await message
     /**try {
     let datcov = await fetch('https://latam-api.vercel.app/api/covid19?apikey=nekosmic&q=world');
