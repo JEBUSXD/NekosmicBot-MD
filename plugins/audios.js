eae03053fb29e55c364c08540b9d0ff016c9fbdf
Yesenia57 <113145347+Yesenia57@users.noreply.github.com>
2022-09-25 14:23:47 -0500

    Update audios.js

--- plugins/audios.js
+++ plugins/audios.js
@@ -1,6 +1,5 @@
 if  ( ! chat . isBanned  &&  chat . audios  &&  m . text . match ( / ( onichan ) / gi ) )  {    
 let  vn  =  '../multimedia/sonidos/onich.mp3'
-conn.sendMessage(m.chat, { audio: { url: vn }, contextInfo: { "externalAdReply": { "title": `onichan >~<`, "body": `Matt_Mdz UwU`, "previewType": "PHOTO", "thumbnailUrl": null,"thumbnail": imagen1, "sourceUrl": `https://youtube.com/channel/UC_Pp8pT9vbT15k5_-i6oseA`
 esto _ sendPresenceUpdate ( 'grabación' ,  m . chat )   
 esto _ sendFile ( m . chat ,  vn ,  'error.mp3' ,  null ,  m ,  true ,  { type : 'audioMessage' ,  ptt : true } ) }
     
